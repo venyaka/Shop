9065c0a66be66ca16742ea9952f2378b96e9c7c5
Veniamin <ilkovveniamin@gmail.com>
2025-08-30 16:13:25 +0500

    build: create project

---
+++ src/test/java/veniamin/shop/backend/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package veniamin.shop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
