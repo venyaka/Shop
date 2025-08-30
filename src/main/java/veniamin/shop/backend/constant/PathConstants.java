17ac3dde5d47509f124f6b6e724ec9e84a35584e
Veniamin <ilkovveniamin@gmail.com>
2025-08-30 18:44:44 +0500

    feat: add constants for IP addresses and API paths

---
+++ src/main/java/veniamin/shop/backend/constant/PathConstants.java
@@ -0,0 +1,6 @@
+package veniamin.shop.backend.constant;
+
+public class PathConstants {
+    public static final String AUTHORIZE_CONTROLLER_PATH = "/authorize";
+    public static final String PRODUCT_CONTROLLER_PATH = "/products";
+}
