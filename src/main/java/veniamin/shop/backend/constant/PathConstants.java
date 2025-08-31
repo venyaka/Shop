544f0dd385ab785400b8febced7b5a047b9459c5
Veniamin <ilkovveniamin@gmail.com>
2025-08-31 19:25:21 +0500

    feat: update API paths in PathConstants for authorization and product management

--- src/main/java/veniamin/shop/backend/constant/PathConstants.java
+++ src/main/java/veniamin/shop/backend/constant/PathConstants.java
@@ -1,6 +1,7 @@
 package veniamin.shop.backend.constant;
 
 public class PathConstants {
-    public static final String AUTHORIZE_CONTROLLER_PATH = "/authorize";
-    public static final String PRODUCT_CONTROLLER_PATH = "/products";
+    public static final String AUTHORIZE_CONTROLLER_PATH = "/api/authorize";
+    public static final String PRODUCT_CONTROLLER_PATH = "/api/products";
+    public static final String PRODUCT_CATEGORY_CONTROLLER_PATH = "/api/categories";
 }
